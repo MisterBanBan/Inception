c7265e4e357145796d0ec01b0cff777fbc724c42
MisterBanBan <favier.alban@gmail.com>
2025-02-17 12:54:45 +0100

    update creat a file data in desktop

--- Makefile
+++ Makefile
@@ -1,12 +1,22 @@
 PROJECT_NAME = inception
 
 build:
-	  docker-compose -f srcs/docker-compose.yml build
+	mkdir -p /home/afavier/data/wordpress
+	mkdir -p /home/afavier/data/mariadb
+	sudo docker-compose -f srcs/docker-compose.yml build
 
-up:
-	  docker-compose -f srcs/docker-compose.yml up -d
+up: build
+	sudo docker-compose -f srcs/docker-compose.yml up
+
+
+.PHONY :build up clean down logs
 
 down:
-	  docker-compose -f srcs/docker-compose.yml down --volumes
+	sudo docker-compose -f srcs/docker-compose.yml down --volumes
+
+clean: down
+	sudo rm -rf /home/afavier/data/wordpress
+	sudo rm -rf /home/afavier/data/mariadb
+
 logs:
 	docker-compose -f srcs/docker-compose.yml logs -f mariadb
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -13,27 +13,27 @@ if [ -f "/var/www/html/wp-config.php" ]; then
     echo "WordPress already initialized"
 else
     echo "Initializing WordPress..."
+
+    wp core download --allow-root
+    
     wp config create --allow-root \
         --dbname=$MYSQL_DATABASE \
         --dbuser=$MYSQL_USER \
         --dbpass=$MYSQL_PASSWORD \
-        --dbhost=$MYSQL_HOST:3306 \
-        --path='/var/www/html'
+        --dbhost=$MYSQL_HOST:3306
 
     wp core install --allow-root \
         --url=$WP_URL \
         --title="$WP_TITLE" \
         --admin_user=$WP_ADMIN_USER \
         --admin_password=$WP_ADMIN_PASSWORD \
-        --admin_email=$WP_ADMIN_EMAIL \
-        --path='/var/www/html'
+        --admin_email=$WP_ADMIN_EMAIL
 
     wp user create --allow-root \
         $WP_USER \
         $WP_USER_EMAIL \
         --user_pass=$WP_USER_PASSWORD \
-        --role=author \
-        --path='/var/www/html'
+        --role=author
 
     echo "WordPress setup complete"
 fi
